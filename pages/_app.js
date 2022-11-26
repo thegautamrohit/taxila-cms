aa1e0e5803f182f3d7d0c564378cf953e558d4b4
thegautamrohit <rgautam1997@gmail.com>
2022-11-26 20:18:59 +0530

    home

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+const ButtonAppBar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Taxila CMS
+          </Typography>
+          <Button color="inherit">Logout</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ButtonAppBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/home.js
@@ -0,0 +1,110 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import CategoryIcon from "@mui/icons-material/Category";
+import CountertopsIcon from "@mui/icons-material/Countertops";
+import AppsIcon from "@mui/icons-material/Apps";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+
+function home() {
+  const cards = [
+    {
+      id: 1,
+      icon: <HomeRoundedIcon fontSize={"large"} style={{ color: "#1976d2" }} />,
+      title: "Home Page",
+      url: "/home",
+    },
+    {
+      id: 2,
+      icon: <CategoryIcon fontSize={"large"} style={{ color: "#1976d2" }} />,
+      title: "Add Product",
+      url: "/home",
+    },
+    {
+      id: 3,
+      icon: <CountertopsIcon fontSize={"large"} style={{ color: "#1976d2" }} />,
+      title: "Kitchen Section",
+      url: "/home",
+    },
+    {
+      id: 4,
+      icon: <AppsIcon fontSize={"large"} style={{ color: "#1976d2" }} />,
+      title: "Inspiration",
+      url: "/home",
+    },
+
+    {
+      id: 6,
+      icon: <LiveTvIcon fontSize={"large"} style={{ color: "#1976d2" }} />,
+      title: "Media",
+      url: "/home",
+    },
+
+    {
+      id: 8,
+      icon: (
+        <CloudDownloadIcon fontSize={"large"} style={{ color: "#1976d2" }} />
+      ),
+      title: "Download",
+      url: "/home",
+    },
+    {
+      id: 9,
+      icon: <NewspaperIcon fontSize={"large"} style={{ color: "#1976d2" }} />,
+      title: "Blogs",
+      url: "/home",
+    },
+  ];
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "grid",
+        rowGap: "30px",
+        gridTemplateColumns: "auto auto auto",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        margin: "50px auto",
+      }}
+    >
+      {cards?.map((card) => (
+        <Card
+          key={card?.id}
+          sx={{ maxWidth: 345 }}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CardActionArea>
+            <CardContent
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Typography gutterBottom variant="h5" component="div">
+                {card?.icon}
+              </Typography>
+              <Typography variant="body" color="text.primary">
+                {card?.title}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default home;
