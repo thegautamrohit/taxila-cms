a88af5276164d7046d301318b1f8e509122f5da6
ajay upadhyay <ajay.upadhyay@redcliffelabs.com>
2022-11-29 01:09:35 +0530

    login api done

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,27 +1,26 @@
 import query from "../../lib/connectDb";
 import jwt from "jsonwebtoken";
-import moment from "moment";
-import { v4 as uuidv4 } from "uuid";
 export default async function handler(req, res) {
   try {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const user = { name: username };
-    const jwtToken = jwt.sign(
-      { exp: Math.floor(Date.now() / 1000) + 60 * 60, user },
-      process.env.ACCESS_TOKEN_SECRET
-    );
-
-    const sql = `INSERT INTO Users (id, createdAt, email) VALUES("123254","username")`;
     const valueParams = [];
+    const email = req?.body?.email;
+    const password = req?.body?.password;
+    if (req?.body?.email && req?.body?.password) {
+      const sql = `SELECT email , password FROM Users WHERE email = "${email}" AND password = "${password}";`;
+      const [result] = await query({ query: sql, value: valueParams });
+      if (result) {
+        const user = { name: email };
+        const jwtToken = jwt.sign(
+          { exp: Math.floor(Date.now() / 1000) + 60 * 60, user },
+          process.env.ACCESS_TOKEN_SECRET
+        );
 
-    const result = await query({ query: sql, value: valueParams });
-
-    console.log(result);
-
-    res.status(200).json({ result: jwtToken });
+        res.status(200).json({ token: jwtToken });
+      }
+      res.status(400).json({ error: "No user found with this Username" });
+    }
+    res.status(400).json({ error: "Please fill Username or Password" });
   } catch (error) {
-    // res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 }
