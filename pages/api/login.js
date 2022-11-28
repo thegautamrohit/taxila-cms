e275e4688f6470fc4c50fd960bb8019ef88c6616
Ajay Upadhyay <ajay.bhumca18@gmail.com>
2022-11-28 18:05:37 +0530

    api added

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,12 +1,25 @@
 import query from "../../lib/connectDb";
-
+import jwt from "jsonwebtoken";
+import moment from "moment";
+import { v4 as uuidv4 } from "uuid";
 export default async function handler(req, res) {
   try {
-    const sql = "SELECT * FROM material";
-    const valueParams = [10];
-    const [result] = await query({ query: sql, value: valueParams });
-    res.status(200).json({ result: result });
+    const username = req.body.username;
+    const user = { name: username };
+    const jwtToken = jwt.sign(
+      { exp: Math.floor(Date.now() / 1000) + 60 * 60, user },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const sql = `INSERT INTO Users (id, createdAt, email) VALUES("123254","username")`;
+    const valueParams = [];
+
+    const result = await query({ query: sql, value: valueParams });
+
+    console.log(result);
+
+    res.status(200).json({ result: jwtToken });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    // res.status(500).json({ error: error.message });
   }
 }
