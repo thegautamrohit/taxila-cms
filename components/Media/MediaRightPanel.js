4a6b862675720a1e25298bd3888619bbb0e96ab3
thegautamrohit <rgautam1997@gmail.com>
2023-01-11 16:07:26 +0530

    media update

---
+++ components/Media/MediaRightPanel.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import { Drawer } from "@mui/material";
+
+function MediaRightPanel() {
+  return (
+    <div>
+      <Drawer sx={{ width: "100%", marginTop: "75px" }}>
+        <h1>Heading</h1>
+      </Drawer>
+    </div>
+  );
+}
+
+export default MediaRightPanel;
--- pages/media2/index.js
+++ pages/media2/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MediaLeftPanel from "../../components/Media/MediaLeftPanel";
+import MediaRightPanel from "../../components/Media/MediaRightPanel";
 
 function Index() {
   return (
     <div>
       <MediaLeftPanel />
+      <MediaRightPanel />
     </div>
   );
 }
